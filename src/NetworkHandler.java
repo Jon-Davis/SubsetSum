be4e4bf5f09fc8620f6b3aaee89a725ebc46003d
Jonathon Davis <jonathondevindavis@gmail.com>
2017-04-30 15:14:56 -0700

    Auto refresh selector

--- src/NetworkHandler.java
+++ src/NetworkHandler.java
@@ -23,14 +23,15 @@ public class NetworkHandler extends Thread {
 	private Selector selector;
 	private NetworkLedger ledger;
 	private String address;
-	private LinkedList<SocketChannel> pendingConnections;
+	private LinkedList<SocketChannel> sockets;
+	private ServerSocketChannel listener;
 
 	public NetworkHandler(int numberOfProcessors) {
 		ledger = new NetworkLedger();
-		pendingConnections = new LinkedList<>();
+		sockets = new LinkedList<>();
 		try {
 			selector = Selector.open();
-			ServerSocketChannel listener = ServerSocketChannel.open();
+			listener = ServerSocketChannel.open();
 			listener.socket().bind(new InetSocketAddress(NetworkHandler.port));
 			listener.configureBlocking(false);
 			listener.register(this.getSelector(), SelectionKey.OP_ACCEPT);
@@ -43,88 +44,111 @@ public class NetworkHandler extends Thread {
 
 	@Override
 	public void run() {
-		while(SubsetSum.isRunning()){
+		while (SubsetSum.isRunning()) {
 			int num = 0;
+
+			try {
+				selector.close();
+				selector = Selector.open();
+				listener.register(selector, SelectionKey.OP_ACCEPT);
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
 			
-			while(pendingConnections.size() > 0){
+			for(SocketChannel socket : sockets) {
 				try {
-					pendingConnections.getFirst().configureBlocking(false);
-					pendingConnections.removeFirst().register(selector, SelectionKey.OP_READ);
+					socket.configureBlocking(false);
+					socket.register(selector, SelectionKey.OP_READ);
 				} catch (ClosedChannelException e1) {
 					e1.printStackTrace();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
+
 			try {
 				num = selector.select();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			if(num==0)
+
+			if (num == 0)
 				continue;
-			
+
 			Set keys = selector.selectedKeys();
 			Iterator it = keys.iterator();
-			while (it.hasNext()){
-				SelectionKey key = (SelectionKey)it.next();
-				if(!key.isAcceptable())
-					continue;
-				if(key.interestOps()==SelectionKey.OP_ACCEPT){
-					//register the new connection with the Selector
+			while (it.hasNext()) {
+				SelectionKey key = (SelectionKey) it.next();
+				if (key.interestOps() == SelectionKey.OP_ACCEPT && key.isAcceptable()) {
+					// register the new connection with the Selector
 					try {
-						SocketChannel sc = ((ServerSocketChannel) key.channel()).accept();
-						sc.configureBlocking(false);
-						sc.register(this.getSelector(), SelectionKey.OP_READ);
+						sockets.add(((ServerSocketChannel) key.channel()).accept());
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-				} else if (key.interestOps()==SelectionKey.OP_READ){
-					//read the message from the socket
-					read((SocketChannel) key.channel());
+				} else if (key.interestOps() == SelectionKey.OP_READ) {
+					// read the message from the socket
+					SocketChannel readChannel = (SocketChannel) key.channel();
+					key.cancel();
+					try {
+						readChannel.configureBlocking(true);
+						read(readChannel);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
-			
+
 			keys.clear();
 		}
-		
+
 	}
-	
-	public void read(SocketChannel selectableChannel){
-		System.out.println("reading");
-		 try {
+
+	public void read(SocketChannel selectableChannel) {
+		try {
+			selectableChannel.configureBlocking(true);
 			ObjectInputStream ois = new ObjectInputStream(selectableChannel.socket().getInputStream());
-			ObjectOutputStream  oos = new ObjectOutputStream(selectableChannel.socket().getOutputStream());
+			ObjectOutputStream oos = new ObjectOutputStream(selectableChannel.socket().getOutputStream());
 			Message message = (Message) ois.readObject();
-			//determine the type of message 
-			if(message.type == Message.CONNECT){
-				ledger.addHost((Integer) message.argument, selectableChannel.socket().getRemoteSocketAddress().toString(), selectableChannel);
-				//TODO: Send a Message of Type HOST_INFO, with this computers info
+			// determine the type of message
+			if (message.type == Message.CONNECT) {
+				ledger.addHost((Integer) message.argument,
+						selectableChannel.socket().getRemoteSocketAddress().toString(), selectableChannel);
+				// TODO: Send a Message of Type HOST_INFO, with this computers
+				// info
 			} else if (message.type == Message.NEW_CONNECTION) {
-				ledger.addHost((Integer) message.argument, selectableChannel.socket().getRemoteSocketAddress().toString(), selectableChannel);
-				// TODO: Compile list of other machines in the network, send this in a message of type NETWORK_INFO
+				String id = selectableChannel.socket().getRemoteSocketAddress().toString().split("/")[1].split(":")[0];
+				ledger.addHost((Integer) message.argument,id, selectableChannel);
+				// TODO: Compile list of other machines in the network, send
+				// this in a message of type NETWORK_INFO
 				System.out.println("Recieved new connection from " + selectableChannel.socket().getRemoteSocketAddress().toString());
 			} else if (message.type == Message.NETWORK_INFO) {
-				// TODO: Given a list of NETWORK_INFO connect to, and collect information from all other hosts in the network
-			} else if (message.type == Message.RUN){
-				// TODO: Given a list of numbers to compute, 
-				// calculate the permutations this host is responsible for given the NetworkLedger
+				// TODO: Given a list of NETWORK_INFO connect to, and collect
+				// information from all other hosts in the network
+			} else if (message.type == Message.RUN) {
+				// TODO: Given a list of numbers to compute,
+				// calculate the permutations this host is responsible for given
+				// the NetworkLedger
 				// Then run the calculations
 			} else if (message.type == Message.NOTIFY) {
-				// TODO: The src of this message is notifying this host that it has completed all of its assigned work
+				// TODO: The src of this message is notifying this host that it
+				// has completed all of its assigned work
 			} else if (message.type == Message.REQUEST) {
-				
-			} else if (message.type == Message.REQUEST_RESPONSE){
-				
+
+			} else if (message.type == Message.REQUEST_RESPONSE) {
+
 			}
 		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
+			String id = selectableChannel.socket().getRemoteSocketAddress().toString().split("/")[1].split(":")[0];
+			System.out.println("Closing connection with " + id);
+			sockets.remove(selectableChannel);
+			try {
+				selectableChannel.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
-	
 
 	/**
 	 * @return the selector
@@ -132,25 +156,25 @@ public class NetworkHandler extends Thread {
 	public Selector getSelector() {
 		return selector;
 	}
-	
-	public void connect(String ipAddress, int numberOfProcessors){
+
+	public void connect(String ipAddress, int numberOfProcessors) {
 		System.out.println("opening connections with " + ipAddress);
 		SocketChannel socketChannel;
 		try {
 			socketChannel = SocketChannel.open();
 			socketChannel.configureBlocking(true);
 			socketChannel.connect(new InetSocketAddress(ipAddress, port));
-			pendingConnections.add(socketChannel);
-			ObjectOutputStream  oos = new ObjectOutputStream(socketChannel.socket().getOutputStream());
+			sockets.add(socketChannel);
+			ObjectOutputStream oos = new ObjectOutputStream(socketChannel.socket().getOutputStream());
 			Message newConnect = new Message(this.address, ipAddress, Message.NEW_CONNECTION, numberOfProcessors);
 			oos.writeObject(newConnect);
 			selector.wakeup();
 			System.out.println("Established connection with " + ipAddress);
 		} catch (IOException e) {
-			System.out.println("Failed to connect to " +ipAddress);
+			System.out.println("Failed to connect to " + ipAddress);
 			return;
 		}
-		
+
 	}
-	
+
 }
